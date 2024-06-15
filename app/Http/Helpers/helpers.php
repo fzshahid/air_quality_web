27b08483bee2d5ef09b2041edb611d71eebad2ab
faixan23 <faixan23@gmail.com>
2024-06-15 16:22:47 +0200

    alert subscription email added

--- app/Http/Helpers/helpers.php
+++ app/Http/Helpers/helpers.php
@@ -7,3 +7,9 @@ const DATE_TIME_FORMAT = 'Y-m-d H:i:s';
 const PAGINATION_LIMIT = 15;
 const NTH_SPONSOR_LIMIT = 3;
 const URL_REGEX = 'regex:/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/';
+
+
+function notifyUsers() {
+    $a = \Brackets\AdminAuth\Models\AdminUser::all();
+    \Notification::send($a, new \App\Notifications\UserSubscribed());
+}
