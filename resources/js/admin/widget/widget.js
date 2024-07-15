16ed092aa5ee51e8ae71b7f4fc58562722ed6d27
faixan23 <faixan23@gmail.com>
2024-07-15 02:50:04 +0200

    widget

--- resources/js/admin/widget/widget.js
+++ resources/js/admin/widget/widget.js
@@ -1,38 +1,98 @@
-import axios from 'axios';
-import AppForm from '../app-components/Form/AppForm';
-
 Vue.component('widget', {
-    mixins: [AppForm],
-    component: {
-        // 'CommitChart': CommitChart,
-        //   'bar-chart-container': BarChartContainer,
-        //   'line-chart-container': LineChartContainer,
+    // mixins: [AppListing],
+    props: {  },
+    data: function () {
+        return {
+            form: {
+                startDate: moment().subtract('7', 'days').format('YYYY-MM-DD'),
+                endDate: moment().format('YYYY-MM-DD'),
+            },
+            isLoading: false,
+            keyLabels: {
+                humidity: {
+                    label: 'Humidity',
+                    url: 'dashboard/line-chart-humidity',
+                    xLabel: 'Time',
+                    yLabel: 'Percentage',
+                    unit: '%',
+                },
+                temperature: {
+                    label:  'Temperature',
+                    url: 'dashboard/line-chart-temperature',
+                    xLabel: 'Time',
+                    yLabel: 'Celcius',
+                    unit: '°C',
+                },
+                pm1_5: {
+                    label:  'PM1.5',
+                    url: 'dashboard/line-chart-pm-1-5',
+                    xLabel: 'Time',
+                    yLabel: 'µg/m³',
+                    unit: 'µg/m³',
+                },
+                pm2_5: {
+                    label:  'PM2.5',
+                    url: 'dashboard/line-chart-pm-2-5',
+                    xLabel: 'Time',
+                    yLabel: 'µg/m³',
+                    unit: 'µg/m³',
+                },
+                pm4: {
+                    label:  'PM4',
+                    url: 'dashboard/line-chart-pm-4',
+                    xLabel: 'Time',
+                    yLabel: 'µg/m³',
+                    unit: 'µg/m³',
+                },
+                pm10: {
+                    label:  'PM10',
+                    url: 'dashboard/line-chart-pm-10',
+                    xLabel: 'Time',
+                    yLabel: 'µg/m³',
+                    unit: 'µg/m³',
+                },
+                tvoc: {
+                    label:  'TVOC',
+                    url: 'dashboard/line-chart-tvoc',
+                    xLabel: 'Time',
+                    yLabel: 'ppb',
+                    unit: 'ppb',
+                },
+                co2: {
+                    label:  'CO₂',
+                    url: 'dashboard/line-chart-co-2',
+                    xLabel: 'Time',
+                    yLabel: 'Parts per million',
+                    unit: 'ppm',
+                },
+                eco2: {
+                    label:  'eCO₂',
+                    url: 'dashboard/line-chart-co-2',
+                    xLabel: 'Time',
+                    yLabel: 'Parts per million',
+                    unit: 'ppm',
+                },
+                all: {
+                    label:  'All Metrices',
+                    url: 'dashboard/line-chart-co-2',
+                    xLabel: 'Time',
+                    yLabel: '',
+                    unit: '',
+                },
+            },
+            showModal: false,
+            email: '',
+            aqiData: {},
+            selectedItem: {},
+            chartKey: +new Date(),
+        }
     },
-    props: {
-        // statuses: {
-        //     type: Array,
-        //     required: true,
-        // },
-    },
-    data: {
-        keyLabels: {
-            humidity: 'Humidity',
-            temperature: 'Temperature',
-            pm1_5: 'PM1.5',
-            pm10: 'PM10',
-            tvoc: 'TVOC',
-            co2: 'CO₂'
-        },
-        units: {
-            humidity: '%',
-            temperature: '°C',
-            pm1_5: 'µg/m³',
-            pm10: 'µg/m³',
-            tvoc: 'ppb',
-            co2: 'ppm'
-        },
-        showModal: false,
-        email: ''
+    async mounted() {
+        if (this.apiData) {
+            this.aqiData = this.apiData;
+        }
+
+        await this.loadData();
     },
     watch: {
         showModal(newValue) {
@@ -44,6 +104,23 @@ Vue.component('widget', {
         }
     },
     methods: {
+        switchChart(item) {
+            this.selectedItem = item;
+            this.chartKey++;
+        },
+        loadData: async function () {
+            try {
+                const apiUrl = "/api/get-widget-data";
+                const data = await axios.get(
+                    apiUrl
+                );
+                
+                this.aqiData = data.data.data;
+                this.loaded = true;
+            } catch (e) {
+                console.error(e);
+            }
+        },
         subscribe() {
             if (this.email === '') {
                 this.$toasted.show("Please enter a valid email address", {
